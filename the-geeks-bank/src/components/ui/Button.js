5f475f5649b546a86aea06314d231002ed019996
6abrielMR <gabriel.montes716@gmail.com>
2022-03-29 22:16:41 -0500

    Creation of button component

---
+++ the-geeks-bank/src/components/ui/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = () => {
+  return (
+    <div className="btnc select-disable normal d-flex justify-content-center align-items-center">
+      1
+    </div>
+  );
+};
